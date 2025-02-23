6c5ecf4951a00cee86f32c2a601c1f5508d3e3d7
JGoldman95 <jagold1995@gmail.com>
2025-02-23 17:32:12 -0500

    Add movement methods to Player, modify main to call player in container

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     asteroid_game_clock = pygame.time.Clock()
     dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -17,8 +21,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill((0,0,0,0))
-        player.draw(screen)
+        screen.fill((0,0,0))
+        for sprite in updatable:
+            sprite.update(dt)
+
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
 
         #framerate limit
