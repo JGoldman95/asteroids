8af89ddb83ef5309e95626b94020a3e24c71cc6e
JGoldman95 <jagold1995@gmail.com>
2025-02-23 15:08:58 -0500

    Modify 'main.py' to include fps cap, tracker

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from constants import *
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    asteroid_game_clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -17,6 +17,9 @@ def main():
         screen.fill((0,0,0,0))
         pygame.display.flip()
 
+        #framerate limit
+        dt = asteroid_game_clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
