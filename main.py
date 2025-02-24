3e0c82e5df2ceb13081c0ed4d0944239c001016d
JGoldman95 <jagold1995@gmail.com>
2025-02-23 21:11:38 -0500

    Modify shooting to include collision, asteroid splitting

--- main.py
+++ main.py
@@ -39,6 +39,10 @@ def main():
                 if asteroid.collision(player):
                     print("Game over!")
                     exit()
+                for shot in shots:
+                    if asteroid.collision(shot):
+                        shot.kill()
+                        asteroid.split()
 
 
         for sprite in drawable:
